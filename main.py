86fbc0f3dad93cb564c319026513e9005fc2aa1c
MDavidka <117516603+MDavidka@users.noreply.github.com>
2025-03-10 19:38:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ from spotipy.oauth2 import SpotifyOAuth
 import os
 import time
 import threading
+from flask_session import Session
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
+
+# Session configuration
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # MongoDB configuration
 app.config["MONGO_URI"] = "mongodb+srv://EFmTCpVa57UnGnG1:EFmTCpVa57UnGnG1@cluster0.iuxk8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
@@ -16,7 +21,7 @@ mongo = PyMongo(app)
 # Spotify API credentials
 SPOTIPY_CLIENT_ID = '3baa3b2f48c14eb0b1ec3fb7b6c5b0db'
 SPOTIPY_CLIENT_SECRET = '62f4ad9723464096864224831ed841b3'
-SPOTIPY_REDIRECT_URI = 'https://ltpd.xyz/callback'  # Change this to your actual redirect URI
+SPOTIPY_REDIRECT_URI = 'https://ltpd.xyz/callback'
 
 sp_oauth = SpotifyOAuth(
     SPOTIPY_CLIENT_ID,
@@ -72,6 +77,9 @@ def stats():
     token_info = session.get("token_info", None)
     if not token_info:
         return redirect(url_for('login'))
+    if sp_oauth.is_token_expired(token_info):
+        token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
+        session["token_info"] = token_info
     sp = Spotify(auth=token_info['access_token'])
     user_profile = sp.current_user()
     user_data = mongo.db.users.find_one({"user_id": user_profile['id']})
