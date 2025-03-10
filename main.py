c6790874aa8f5f6813244709b18e9698f03f6dc9
MDavidka <117516603+MDavidka@users.noreply.github.com>
2025-03-10 19:29:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask_pymongo import PyMongo
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
 import os
+import time
+import threading
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -14,7 +16,7 @@ mongo = PyMongo(app)
 # Spotify API credentials
 SPOTIPY_CLIENT_ID = '3baa3b2f48c14eb0b1ec3fb7b6c5b0db'
 SPOTIPY_CLIENT_SECRET = '62f4ad9723464096864224831ed841b3'
-SPOTIPY_REDIRECT_URI = 'https://ltpd.xyz/callback'  # Change this to your actual redirect URI
+SPOTIPY_REDIRECT_URI = 'http://localhost:5000/callback'  # Change this to your actual redirect URI
 
 sp_oauth = SpotifyOAuth(
     SPOTIPY_CLIENT_ID,
@@ -22,6 +24,31 @@ sp_oauth = SpotifyOAuth(
     SPOTIPY_REDIRECT_URI,
     scope="user-read-private user-read-playback-state user-read-currently-playing"
 )
+
+def update_listening_time():
+    while True:
+        with app.app_context():
+            token_info = session.get("token_info", None)
+            if token_info:
+                sp = Spotify(auth=token_info['access_token'])
+                playback = sp.current_playback()
+                if playback and playback['is_playing']:
+                    track_id = playback['item']['id']
+                    user_id = sp.current_user()['id']
+                    user_data = mongo.db.users.find_one({"user_id": user_id})
+                    if not user_data:
+                        mongo.db.users.insert_one({"user_id": user_id, "total_minutes": 0})
+                    else:
+                        if 'last_track_id' in user_data and user_data['last_track_id'] != track_id:
+                            track_duration_ms = playback['item']['duration_ms']
+                            minutes_played = track_duration_ms // 60000
+                            mongo.db.users.update_one(
+                                {"user_id": user_id},
+                                {"$inc": {"total_minutes": minutes_played}, "$set": {"last_track_id": track_id}}
+                            )
+            time.sleep(60)
+
+threading.Thread(target=update_listening_time).start()
 
 @app.route('/')
 def index():
@@ -47,8 +74,9 @@ def stats():
         return redirect(url_for('login'))
     sp = Spotify(auth=token_info['access_token'])
     user_profile = sp.current_user()
-    current_playback = sp.current_playback()
-    return render_template('stats.html', user=user_profile, playback=current_playback)
+    user_data = mongo.db.users.find_one({"user_id": user_profile['id']})
+    total_minutes = user_data['total_minutes'] if user_data else 0
+    return render_template('stats.html', user=user_profile, total_minutes=total_minutes)
 
 if __name__ == '__main__':
     app.run(debug=True)
